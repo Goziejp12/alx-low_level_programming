36d5df0da61517b65749b0891cb3ad57f2beee94
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-08 14:17:26 -0700

    committing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-	int i;
 	char c;
-	float f;
+	int i;
 	long int l;
 	long long int l2;
+	float f;
 
-	printf("Size of an int: %i byte(s)\n", sizeof(i));
 	printf("Size of a char: %i byte(s)\n", sizeof(c));
-	printf("Size of a float: %i byte(s)\n", sizeof(f));
+	printf("Size of an int: %i byte(s)\n", sizeof(i));
 	printf("Size of a long int: %i byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(l2));
+	printf("Size of a float: %i byte(s)\n", sizeof(f));
 	return (0);
 }
