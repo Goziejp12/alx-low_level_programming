72d76fef47c48c16bdf4adfa9a1713b82695befb
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-08 10:47:01 -0700

    committing

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - "prints the size of various types 
+ * on the computer it is compiled and run on"
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+	float f;
+	long l;
+	long long l2;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of a float is: %lu.\n", (unsigned long)sizeof(f));
+	printf("The size of a long is: %lu.\n", (unsigned long)sizeof(l));
+	printf("The size of a long long: %lu.\n", (unsigned long)sizeof(l2));
+
+	return (0);
+}
