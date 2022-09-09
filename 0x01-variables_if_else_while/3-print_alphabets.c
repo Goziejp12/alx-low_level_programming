7d29846fffa3a57cc5f80a02952407253dd261b1
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-09 15:23:14 -0700

    committing

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,15 @@ int main(void)
 	char ch_1 = 'a';
 	char ch_2 = 'A';
 	
-	while (ch_1 <= 'z' && ch_2 <= 'Z')
+	while (ch_1 <= 'z')
 	{
 		putchar(ch_1);
+		ch_1++;
+	}
+	while (ch_2 <= 'Z')
+	{
 		putchar(ch_2);
+		ch_2++;
 	}
 	putchar('\n');
 	return (0);
