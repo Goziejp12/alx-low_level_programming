3234525caa5ab05a13a09f0bc9dc907a514a0b44
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-09 16:02:48 -0700

    committing

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,16 @@ int main(void)
 {
 	char ch = 'a';
 
-	while (ch <= 'z' || ch != 'q' || ch != 'e')
+	while (ch <= 'z')
+	{
+	if (ch != 'q' && ch != 'e')
 	{
 		putchar(ch);
 		ch++;
+	}
+	else
+	{
+	}
 	}
 	putchar('\n');
 	return (0);
