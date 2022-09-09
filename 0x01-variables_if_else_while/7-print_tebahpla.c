67078f6beac74e8de9ff7716746be572b513f521
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-09 16:55:46 -0700

    committing

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet in reverse
+ * Return: (0)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	for (ch = 'z'; ch => 'a', ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
