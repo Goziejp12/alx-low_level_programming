2679576dfa40e9eed1c15a57d22045c195ead641
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-09 17:25:23 -0700

    committing

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-	char h;
+	char h, h2;
 
 	for (h = 0; h < 10; h++)
 	{
 		putchar(h);
 	}
-	for (h = 'a'; h < 'g'; h++)
+	for (h2 = 'a'; h2 < 'g'; h2++)
 	{
-		putchar(h);
+		putchar(h2);
 	}
 	putchar('\n');
 	return (0);
