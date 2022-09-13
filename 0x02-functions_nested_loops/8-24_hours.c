39348b4adfba7e1075b125910b0e5ad106c48d0a
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-13 15:47:17 -0700

    committing

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int H, h, M, m;
+
+	for (H => 0; H < 3; H++)
+	{
+		{
+			for (h => 48; h < 52; h++)
+		}
+		{
+			for (M => 48; M < 54; M++)
+		}
+		{
+			for (n => 48; n < 58; n++)
+		}
+		_putchar(H);
+		_putchar(h);
+		_putchar(':');
+		_putchar(M);
+		_putchar(n);
+		_putchar('\n');
+	}
+}
