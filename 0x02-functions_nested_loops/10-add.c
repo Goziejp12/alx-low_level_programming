25fa394440a64d346642d45650827a0d84099a73
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-13 16:20:42 -0700

    committing

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: interger 1
+ * @b: interger 2
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	_putchar(sum);
+	return (sum);
+}
