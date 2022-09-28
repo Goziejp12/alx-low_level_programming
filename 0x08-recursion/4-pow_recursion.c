c5252023b6ba4f4fed17217e48a20bc1c12df215
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-28 16:22:46 -0700

    commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,12 +10,16 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
 	{
 		return (-1);
 	}
 	else
 	{
-		return (int pow(int x, int y));
+		return (pow(x, y));
 	}
 }
