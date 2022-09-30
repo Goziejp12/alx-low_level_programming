49e95b92cd26af28c6979373aadce6dd62e1c640
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-30 16:01:33 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the nuber of arguments
+ * passed into it
+ * @argc: argument count variable
+ * @argv: character variable
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
