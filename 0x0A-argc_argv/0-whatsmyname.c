b006fa17e5db4a7239ce41c88f51396b29280c4c
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-30 15:48:58 -0700

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - a program that prints its name, followed by a new line.
  * If you rename the program, it will print the new name,
@@ -11,6 +11,6 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	printf("%s\n", *argv);
+	_putchar(*argv);
 	return (0);
 }
