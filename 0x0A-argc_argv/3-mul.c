bacec4f4a1d67d2bf841d76e7ec39ca43fc4035d
Goziejp12 <chigoziemaduka401@gmail.com>
2022-09-30 17:18:28 -0700

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,12 @@
  */
 int main(int argc; char *argv[])
 {
-	int mul = 0;
+	int mul;
 
 	if (argc == 3)
 	{
-		{
 			mul = atoi(argv[1]) * atoi(argv[2]);
 			printf("%d\n", mul);
-		}
 	}
 	else
 	{
