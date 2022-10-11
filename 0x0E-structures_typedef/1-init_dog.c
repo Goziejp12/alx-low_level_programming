092f72f6a3e3a5f8fea60cb273943453972f70a7
Goziejp12 <chigoziemaduka401@gmail.com>
2022-10-10 17:40:20 -0700

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - a function that initializes a struct
+ * @d: member d
+ * @name: name member
+ * @age: age member
+ * @owner: string literal member
+ * Return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog init_dog;
+}
